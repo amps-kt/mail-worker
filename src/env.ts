91b6b1dc3715b9bf6ca46c02e55fb05829a5bd8f
Jacob Trevor <50667410+JakeTrevor@users.noreply.github.com>
2026-04-04 18:29:20 +0100

    updated redis connection to accept host

--- src/env.ts
+++ src/env.ts
@@ -11,8 +11,12 @@ export const env = createEnv({
     MAIL_USER: z.email(),
     /** SMTP password (if applicable) */
     MAIL_PASSWORD: z.string().optional(),
+
+    /** Host for redis instance (for MQ) */
+    REDIS_HOST: z.coerce.string(),
     /** Port for redis instance (for MQ) */
     REDIS_PORT: z.coerce.number(),
+    
     /** number of emails the service is allowed to send per period */
     MAIL_RATE_LIMIT: z.coerce.number(),
     /** Period for rate limit in milliseconds*/
--- src/redis-connection.ts
+++ src/redis-connection.ts
@@ -3,6 +3,7 @@ import IORedis from "ioredis";
 import { env } from "./env";
 
 export const connection = new IORedis({
+  host: env.REDIS_HOST,
   port: env.REDIS_PORT,
   maxRetriesPerRequest: null,
 });
